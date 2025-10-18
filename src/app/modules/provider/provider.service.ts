b64c9203ea4a3255e82a69363ea16cdf8e140ece
manik <maniksarker652@gmail.com>
2025-10-18 17:56:35 +0600

    make changes

--- src/app/modules/provider/provider.service.ts
+++ src/app/modules/provider/provider.service.ts
@@ -1,15 +1,19 @@
-import httpStatus from "http-status";
-import AppError from "../../error/appError";
-import { IProvider } from "./provider.interface";
-import providerModel from "./provider.model";
+import httpStatus from 'http-status';
+import AppError from '../../error/appError';
+import { IProvider } from './provider.interface';
+import providerModel from './provider.model';
 
+// kdfidfidudu
 const updateUserProfile = async (id: string, payload: Partial<IProvider>) => {
     if (payload.email || payload.username) {
-        throw new AppError(httpStatus.BAD_REQUEST, "You cannot change the email or username");
+        throw new AppError(
+            httpStatus.BAD_REQUEST,
+            'You cannot change the email or username'
+        );
     }
     const user = await providerModel.findById(id);
     if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
+        throw new AppError(httpStatus.NOT_FOUND, 'Profile not found');
     }
     return await providerModel.findByIdAndUpdate(id, payload, {
         new: true,
