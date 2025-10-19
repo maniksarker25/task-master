c474f8c84a4e939fe87044275662f0937974cc0d
manik <maniksarker652@gmail.com>
2025-10-19 11:48:32 +0600

    create service for subscriber

--- src/app/modules/subscriber/subscriber.service.ts
+++ src/app/modules/subscriber/subscriber.service.ts
@@ -1,21 +1,20 @@
-import httpStatus from "http-status";
-import AppError from "../../error/appError";
-import { ISubscriber } from "./subscriber.interface";
-import subscriberModel from "./subscriber.model";
+import httpStatus from 'http-status';
+import AppError from '../../error/appError';
+import { ISubscriber } from './subscriber.interface';
+import Subscriber from './subscriber.model';
 
-const updateUserProfile = async (id: string, payload: Partial<ISubscriber>) => {
-    if (payload.email || payload.username) {
-        throw new AppError(httpStatus.BAD_REQUEST, "You cannot change the email or username");
-    }
-    const user = await subscriberModel.findById(id);
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
-    }
-    return await subscriberModel.findByIdAndUpdate(id, payload, {
-        new: true,
-        runValidators: true,
+const createSubscriber = async (payload: ISubscriber) => {
+    const existingSubscriber = await Subscriber.findOne({
+        email: payload.email,
     });
+    if (existingSubscriber) {
+        throw new AppError(
+            httpStatus.CONFLICT,
+            'Subscriber with this email already exists'
+        );
+    }
+    return await Subscriber.create(payload);
 };
 
-const SubscriberServices = { updateUserProfile };
+const SubscriberServices = { createSubscriber };
 export default SubscriberServices;
