80773ae091a0447fccee5f83e2d2a557e1452ad6
manik <maniksarker652@gmail.com>
2025-11-08 10:12:01 +0600

    s

--- src/app/modules/service/service.routes.ts
+++ src/app/modules/service/service.routes.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import { uploadFile } from '../../helper/multer-s3-uploader';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user.constant';
 import serviceController from './service.controller';
 import ServiceValidations from './service.validation';
-import auth from '../../middlewares/auth';
-import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
@@ -34,6 +34,13 @@ router.get(
 );
 router.get(
     '/update-service',
+    uploadFile(),
+    (req, res, next) => {
+        if (req.body.data) {
+            req.body = JSON.parse(req.body.data);
+        }
+        next();
+    },
     auth(USER_ROLE.provider),
     serviceController.updateService
 );
