3503cac6032eefb4bf8b37066db0bdfb4a130555
rashidsarkar <rashidrock558@gmail.com>
2025-11-08 15:10:51 +0600

    error qusction

--- src/app/modules/question/question.controller.ts
+++ src/app/modules/question/question.controller.ts
@@ -5,6 +5,20 @@ import sendResponse from '../../utilities/sendResponse';
 import { getCloudFrontUrl } from '../../helper/multer-s3-uploader';
 import QuestionServices from './question.service';
 
+// const createCategory = catchAsync(async (req, res) => {
+//     const file: any = req.files?.category_image;
+//     if (req.files?.category_image) {
+//         req.body.category_image = getCloudFrontUrl(file[0].key);
+//     }
+//     const result = await categoryService.createCategoryIntoDB(req?.body);
+//     sendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: 'Category created successfully',
+//         data: result,
+//     });
+// });
+
 const createQuestion = catchAsync(async (req, res) => {
     const file: any = req.files?.question_image;
 
