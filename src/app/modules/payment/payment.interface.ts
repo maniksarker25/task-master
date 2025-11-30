64ae71d4894fcaa7a55acfc0aa83d20a2963f754
manik <maniksarker652@gmail.com>
2025-11-30 11:34:54 +0600

    make soem changes on resolve

--- src/app/modules/payment/payment.interface.ts
+++ src/app/modules/payment/payment.interface.ts
@@ -2,11 +2,7 @@ import { Types } from 'mongoose';
 import { ENUM_PAYMENT_STATUS } from './payment.enum';
 
 export interface IPayment {
-    provider: Types.ObjectId; // ref -> Provider
-    email: string;
-    amount: number;
-    bankName: string;
-    acNumber: string;
-    completeDate: Date;
+    provider: Types.ObjectId;
     status: (typeof ENUM_PAYMENT_STATUS)[keyof typeof ENUM_PAYMENT_STATUS];
+    task: Types.ObjectId;
 }
