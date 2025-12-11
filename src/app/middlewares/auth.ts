dfab54f034d77a6cf175f42dfdd92213e06d5ce9
manik <maniksarker652@gmail.com>
2025-12-11 10:56:19 +0600

    make soem changes on auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import AppError from '../error/appError';
+import { Customer } from '../modules/customer/customer.model';
+import { Provider } from '../modules/provider/provider.model';
+import SuperAdmin from '../modules/superAdmin/superAdmin.model';
+import { USER_ROLE } from '../modules/user/user.constant';
 import { TUserRole } from '../modules/user/user.interface';
-import { User } from '../modules/user/user.model';
 import catchAsync from '../utilities/catchasync';
 
 // make costume interface
@@ -45,7 +49,28 @@ const auth = (...requiredRoles: TUserRole[]) => {
                 throw new AppError(httpStatus.UNAUTHORIZED, 'Token is expired');
             }
             // get the user if that here ---------
-            const user = await User.findById(id);
+            // const user = await User.findById(id);
+            let profileData: any;
+            if (role == USER_ROLE.admin) {
+                console.log('nice');
+            } else if (role == USER_ROLE.customer) {
+                profileData = await Customer.findOne({ user: id })
+                    .select('_id user')
+                    .populate({
+                        path: 'user',
+                        select: '_id isDeleted isBlocked isVerified passwordChangedAt',
+                    });
+            } else if (role == USER_ROLE.provider) {
+                profileData = await Provider.findOne({ user: id }).populate({
+                    path: 'user',
+                });
+            } else if (USER_ROLE.superAdmin) {
+                profileData = await SuperAdmin.findOne({ user: id }).populate({
+                    path: 'user',
+                });
+            }
+
+            const { user } = profileData;
             if (!user) {
                 throw new AppError(
                     httpStatus.UNAUTHORIZED,
@@ -88,6 +113,7 @@ const auth = (...requiredRoles: TUserRole[]) => {
             }
             // add those properties in req
             req.user = decoded as JwtPayload;
+            req.user.profileId = profileData._id;
             next();
         }
     );
