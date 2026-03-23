29927c0143dc7001dac1e8d935f56a5162f45e8b
manik <maniksarker652@gmail.com>
2026-03-23 21:40:47 +0600

    added cors for dashboard

--- src/app/socket/socket.ts
+++ src/app/socket/socket.ts
@@ -28,6 +28,7 @@ const initializeSocket = (server: HTTPServer) => {
                     'http://192.168.0.100:3000',
                     'http://10.10.20.48:3000',
                     'https://taskora-website-beryl.vercel.app',
+                    'https://task-master-dashboard-two.vercel.app',
                 ],
             },
         });
